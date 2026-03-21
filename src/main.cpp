d7fbf39041c86248c0912bf1fe946a267d959513
Dor Gidony <dor.gid00@gmail.com>
2026-03-21 17:01:02 +0200

    Switch to rendering two separate triangles with different shaders
    - Introduce \ragmentShaderSource_A\ and \ragmentShaderSource_B\ for distinct colors.
    
    - Create and link two separate shader programs.
    
    - Replace rectangle EBO with two triangle vertex arrays (\erticesA\ and \erticesB\).
    
    - Initialize individual VAOs and VBOs for each triangle.
    
    - Update render loop to switch between shaders and VAOs.

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,20 @@ void main()
     gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
 })";
 
-const char* fragmentShaderSource = R"(#version 330 core
+const char* fragmentShaderSource_A = R"(#version 330 core
 out vec4 FragColor;
 
 void main()
 {
 	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+})";
+
+const char* fragmentShaderSource_B = R"(#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = vec4(0.2f, 0.5f, 1.0f, 1.0f);
 })";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -97,40 +105,65 @@ int main()
 
 	checkShaderCompileResult(vertexShader);
 
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	// Fragment shader A
+	unsigned int fragmentShader_A = glCreateShader(GL_FRAGMENT_SHADER);
 
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
+	glShaderSource(fragmentShader_A, 1, &fragmentShaderSource_A, NULL);
+	glCompileShader(fragmentShader_A);
 
-	checkShaderCompileResult(fragmentShader);
+	checkShaderCompileResult(fragmentShader_A);
 
-	unsigned int shaderProgram = glCreateProgram();
+	// Fragment shader B
+	unsigned int fragmentShader_B = glCreateShader(GL_FRAGMENT_SHADER);
 
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
+	glShaderSource(fragmentShader_B, 1, &fragmentShaderSource_B, NULL);
+	glCompileShader(fragmentShader_B);
 
-	glLinkProgram(shaderProgram);
+	checkShaderCompileResult(fragmentShader_B);
+
+	unsigned int shaderProgram_A = glCreateProgram();
+
+	glAttachShader(shaderProgram_A, vertexShader);
+	glAttachShader(shaderProgram_A, fragmentShader_A);
+
+	glLinkProgram(shaderProgram_A);
+
+	unsigned int shaderProgram_B = glCreateProgram();
+
+	glAttachShader(shaderProgram_B, vertexShader);
+	glAttachShader(shaderProgram_B, fragmentShader_B);
+
+	glLinkProgram(shaderProgram_B);
 
 	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	glDeleteShader(fragmentShader_A);
+	glDeleteShader(fragmentShader_B);
 
-	checkShaderProgramLinkResult(shaderProgram);
+	checkShaderProgramLinkResult(shaderProgram_A);
+	checkShaderProgramLinkResult(shaderProgram_B);
 
 
 	///////////////////////////////////////////////////////////////
 	//////            VAO+VBO+VEO Initialization            ///////
 	///////////////////////////////////////////////////////////////
 
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
+	//constexpr float vertices[] = {
+	//	 0.5f,  0.5f, 0.0f,  // top right
+	//	 0.5f, -0.5f, 0.0f,  // bottom right
+	//	-0.5f, -0.5f, 0.0f,  // bottom left
+	//	-0.5f,  0.5f, 0.0f   // top left 
+	//};
 
-	glBindVertexArray(VAO);
+	constexpr float verticesA[] = {
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	 0.0f,  0.0f, 0.0f  // center
+	};
 
-	constexpr float vertices[] = {
-		 0.5f,  0.5f, 0.0f,  // top right
-		 0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+	constexpr float verticesB[] = {
+	-0.5f,  0.5f, 0.0f,  // top left 
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	 0.0f,  0.0f, 0.0f   // center
 	};
 
 	constexpr unsigned int indices[] = {
@@ -138,21 +171,38 @@ int main()
 		1, 2, 3    // second triangle
 	};
 
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	unsigned int VAO_A;
+	glGenVertexArrays(1, &VAO_A);
+	glBindVertexArray(VAO_A);
+
+	unsigned int VBO_A;
+	glGenBuffers(1, &VBO_A);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_A);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesA), verticesA, GL_STATIC_DRAW);
 
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
 	glEnableVertexAttribArray(0);
 	glBindVertexArray(0);
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	unsigned int VAO_B;
+	glGenVertexArrays(1, &VAO_B);
+	glBindVertexArray(VAO_B);
+
+	unsigned int VBO_B;
+	glGenBuffers(1, &VBO_B);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_B);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesB), verticesB, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	///////////////////////////////////////////////////////////////
 	//////                   Render Loop                    ///////
@@ -166,19 +216,29 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		glUseProgram(shaderProgram_A);
+		glBindVertexArray(VAO_A);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		glUseProgram(shaderProgram_B);
+		glBindVertexArray(VAO_B);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(1, &VAO_A);
+	glDeleteVertexArrays(1, &VAO_B);
+	glDeleteBuffers(1, &VBO_A);
+	glDeleteBuffers(1, &VBO_B);
+
 	glDeleteBuffers(1, &EBO);
-	glDeleteProgram(shaderProgram);
+	glDeleteProgram(shaderProgram_A);
+	glDeleteProgram(shaderProgram_B);
 
 	glfwTerminate();
 	return 0;
