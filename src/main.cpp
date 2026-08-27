211d621c59554c5895d1817bd5dc1ff924872098
Dor Gidony <dor.gid00@gmail.com>
2026-08-27 23:25:40 +0200

    Add multiple rotated qubes

--- src/main.cpp
+++ src/main.cpp
@@ -92,6 +92,18 @@ float vertices[] = {
         -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)
+    };
+
+    float cubeRotation[] = {
+        22.f, 35.f, 66.f, 140.f, 111.f, 227.f, 312.f, 180.f, 299.f, 10.f
+    };
+
     unsigned int indices[] = {
         0, 1, 3, // first triangle
         1, 2, 3 // second triangle
@@ -159,7 +171,26 @@ float vertices[] = {
 
         glBindVertexArray(VAO);
         //glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        //glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // note that we're translating the scene in the reverse direction of where we want to move
+
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(SCR_WIDTH) / SCR_HIEGHT, 0.1f,100.0f);
+
+        shaderProgram.setMat4("view", view);
+        shaderProgram.setMat4("projection", projection);
+
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = cubeRotation[i];
+            model = glm::rotate(model, glm::radians(angle) + static_cast<float>(glfwGetTime()) / (i + 1),glm::vec3(1.0f, 0.3f, 0.5f));
+            shaderProgram.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -175,20 +206,6 @@ float vertices[] = {
             mixCof += 0.01f;
             shaderProgram.setFloat("mixCof", std::max(std::min(mixCof, 1.f), 0.f));
         }
-
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, static_cast<float>(glfwGetTime()) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
-
-        glm::mat4 view = glm::mat4(1.0f);
-        // note that we're translating the scene in the reverse direction of where we want to move
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-        glm::mat4 projection;
-        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-        shaderProgram.setMat4("model", model);
-        shaderProgram.setMat4("view", view);
-        shaderProgram.setMat4("projection", projection);
     }
 
     glDeleteVertexArrays(1, &VAO);
